66a0d979069c5a8d60de819427a85b36a4c9d391
JAVI <javitxin1977@gmail.com>
2024-01-30 11:47:11 +0100

    modificados archivos

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const session = require('express-session');
+const { hashedSecret } = require('../crypto/config');
+const routes = require('./routes');
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(
+    session({
+        secret: hashedSecret,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor en http://localhost:${PORT}`);
+});
